1848e799220e551339d0546a60c6b177b9e7c29c
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-02-28 19:51:54 +0300

    Create index.go

---
+++ index.go
@@ -0,0 +1,7 @@
+package mqtt
+
+import (
+	_ "github.com/pip-services3-go/pip-services3-mqtt-go/build"
+	_ "github.com/pip-services3-go/pip-services3-mqtt-go/connect"
+	_ "github.com/pip-services3-go/pip-services3-mqtt-go/queues"
+)
